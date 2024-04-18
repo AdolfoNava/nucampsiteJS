0375ae227cced5efcd07ba1edd3fea6081b0e287
Adolfo Nava <adolfonava9960@gmail.com>
2024-04-18 18:13:40 -0500

    Toggle play and pause carousel index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
 console.log(`javascript connected!`);
 
 const carousel = new bootstrap.Carousel('#homeCarousel',{
-    interval: 2000,
+    interval: 5000,
     pause: false
 })
+
+const carouselButton = document.getElementById('carouselButton');
+const faIcon = document.getElementById('faButton');
+
+carouselButton.addEventListener('click', () =>{
+    if(faIcon.classList.contains('fa-pause')){
+        faIcon.classList.remove('fa-pause');
+        faIcon.classList.add('fa-play');
+        carousel.pause();
+    }else{
+        faIcon.classList.remove('fa-play');
+        faIcon.classList.add('fa-pause');
+        carousel.cycle();
+    }
+})
+
+/*carousel play pause function v1
 //When user clicks on the pause button, pause the carousel
 const carouselPause = document.getElementById('carouselPause');
 carouselPause.addEventListener('click',() => {
@@ -15,4 +32,4 @@ const carouselPlay = document.getElementById('carouselPlay');
 carouselPlay.addEventListener('click', () =>{
     console.log('cycle the carousel');
     carousel.cycle();
-})
+})*/
